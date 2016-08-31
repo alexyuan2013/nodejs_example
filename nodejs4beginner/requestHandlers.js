927e9d76e649e21e1b2ab392434da7489c1868de
AlexYuan <alexyuan1988@gmail.com>
2016-08-31 16:51:54 +0800

    图片上传和显示

--- nodejs4beginner/requestHandlers.js
+++ nodejs4beginner/requestHandlers.js
@@ -1,13 +1,15 @@
-﻿var exec = require("child_process").exec;
+﻿//var exec = require("child_process").exec;
 var querystring = require("querystring");
+var fs = require("fs");
+var formidable = require("formidable");
 
-function start(response, postData){
+function start(response, request){
 	console.log("Request handler 'start' was called.");
 	//休眠函数
-	function sleep(milliSecond) {
-		var startTime = new Date().getTime();
-		while(new Date().getTime() - startTime < milliSecond);
-	}
+	// function sleep(milliSecond) {
+	// 	var startTime = new Date().getTime();
+	// 	while(new Date().getTime() - startTime < milliSecond);
+	// }
 
 	//var content = "empty";
 
@@ -28,9 +30,9 @@ function start(response, postData){
 		'<meta http-equiv="Content-Type" content="text/html" charset="utf-8"/>' +
 		'</head>' + 
 		'<body>' + 
-		'<form action="/upload" method="post">' + 
-		'<textarea name="text" rows="20" cols="60"></textarea>' +
-		'<input type="submit" value="Submit text"/>' +
+		'<form action="/upload" enctype="multipart/form-data" method="post">' + 
+		'<input type="file" name="upload"/>' +
+		'<input type="submit" value="Upload file"/>' +
 		'</form>' + 
 		'</body>' + 
 		'</html>';
@@ -40,15 +42,41 @@ function start(response, postData){
 
 }
 
-function upload(response, postData){
+function upload(response, request){
 	console.log("Request handler 'upload' was called.");
-	response.writeHead(200, {"Content-Type": "text/plain"});
-	response.write("You have send: " + querystring.parse(postData).text);
-	response.end();
+	var form = new formidable.IncomingForm();
+	console.log("about to parse");
+	form.parse(request, function(error, fields, files){
+		console.log("parsing done");
+		fs.renameSync(files.upload.path, "/tmp/test.png");
+		response.writeHead(200, {"Content-Type":"text/html"});
+		response.write("received image:<br/>");
+		response.write("<img src='/show' />");
+		response.end();
+	});
+
+
+	// response.writeHead(200, {"Content-Type": "text/plain"});
+	// response.write("You have send: " + querystring.parse(postData).text);
+	// response.end();
 	//return "Hello Upload";
 }
 
-
+function show(response, request){
+	console.log("Request handler 'show' was called.");
+	fs.readFile("/tmp/test.png", "binary", function(error, file){
+		if(error){
+			response.writeHead(500, {"Content-Type":"text/plain"});
+			response.write(error + "\n");
+			response.end();
+		} else {
+			response.writeHead(200, {"Content-Type":"image/png"});
+			response.write(file, "binary");
+			response.end();
+		}
+	});
+}
 
 exports.start = start;
 exports.upload = upload;
+exports.show = show;
--- nodejs4beginner/server.js
+++ nodejs4beginner/server.js
@@ -1,24 +1,27 @@
 ﻿//导入nodejs自带的http模块
 var http = require("http");
 var url = require("url");
+var formidable = require("formidable");
 
 function start(route, handle) {
 	function onRequest(request, response){
 		var postData = "";
 		var pathname = url.parse(request.url).pathname;
 		console.log("Request for " + pathname + " received.");
+
+		route(handle, pathname, response, request);
 		//console.log("Request received.");
 		
-		request.setEncoding("utf-8");
+		// request.setEncoding("utf-8");
 
-		request.addListener("data", function(postDataChunk){
-			postData += postDataChunk;
-			console.log("Received POST data chunk: '" + postDataChunk + "'.");
-		});
+		// request.addListener("data", function(postDataChunk){
+		// 	postData += postDataChunk;
+		// 	console.log("Received POST data chunk: '" + postDataChunk + "'.");
+		// });
 
-		request.addListener("end", function(){
-			route(handle, pathname, response, postData);
-		});
+		// request.addListener("end", function(){
+		// 	route(handle, pathname, response, postData);
+		// });
 
 
 		
