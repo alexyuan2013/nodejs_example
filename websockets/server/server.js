b723b69e342b07aaf14c63409fca06f8a41c87d8
AlexYuan <alexyuan1988@gmail.com>
2016-10-13 15:22:50 +0800

    格式化代码

--- websockets/server/server.js
+++ websockets/server/server.js
@@ -1,20 +1,21 @@
 var WebSocketServer = require('ws').Server;
 var PORT = 9087
-var wss = new WebSocketServer({port: PORT});
+var wss = new WebSocketServer({ port: PORT });
 var messages = []; //消息数组
-var CLIENTS=[]; //客户端数组
+var CLIENTS = []; //客户端数组
 
-wss.on('connection', function(ws){
+wss.on('connection', function (ws) {
 
 	//有新连接过来，将当前存储的消息全部发送过去
-	messages.forEach(function(message){
-    ws.send(message);
-  });
+	messages.forEach(function (message) {
+		ws.send(message);
+	});
 
-  CLIENTS.push(ws);
+	CLIENTS.push(ws);
+	
 	ws.on('message', function (message) {
 		messages.push(message); //存储客户端的消息
-		
+
 		console.log('Message Received: %s', message);
 		//以下是两种发送到所有客户端的方式
 		// wss.clients.forEach(function (conn) {
@@ -25,34 +26,34 @@ wss.on('connection', function(ws){
 		sendClient(message); //发送到客户端0
 	});
 
-	
+
 
 	//链接关闭
-	ws.on('close', function() {
-		for (var i=0; i<CLIENTS.length; i++) {
-        if(CLIENTS[i] === ws){
-					CLIENTS.splice(i, 1);
-					console.log('disconnected');
-				}
-    }
+	ws.on('close', function () {
+		for (var i = 0; i < CLIENTS.length; i++) {
+			if (CLIENTS[i] === ws) {
+				CLIENTS.splice(i, 1);
+				console.log('disconnected');
+			}
+		}
 		console.log("Clients online: " + CLIENTS.length);
 	});
 
-	
+
 });
 
 
 
-function sendAll (message) {
-    for (var i=0; i<CLIENTS.length; i++) {
-        CLIENTS[i].send(message);
-    }
+function sendAll(message) {
+	for (var i = 0; i < CLIENTS.length; i++) {
+		CLIENTS[i].send(message);
+	}
 }
 
-function sendClient (message) {
-    if(CLIENTS && CLIENTS.length>0){
-			CLIENTS[0].send(message);
-		}
+function sendClient(message) {
+	if (CLIENTS && CLIENTS.length > 0) {
+		CLIENTS[0].send(message);
+	}
 }
 
 
