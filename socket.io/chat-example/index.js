d0742c3fcdf8aa8ca753d768beb908abdaf92bc9
AlexYuan <alexyuan1988@gmail.com>
2016-10-14 14:04:45 +0800

    添加用户连接队列

--- socket.io/chat-example/index.js
+++ socket.io/chat-example/index.js
@@ -1,6 +1,7 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var users = [];
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html')
@@ -8,15 +9,38 @@ app.get('/', function(req, res){
 
 io.on('connection', function(socket){
   
-  console.log('a user connected');
+  //console.log('a user connected');
 
+  //监听登录事件，将username与websocket连接加入到列表中
+  socket.on('login', function(msg){
+    console.log('user: ' + msg.username + ' login');
+    var user = {};
+    user.name = msg.username;
+    user.socket = socket;
+    users.push(user);
+    console.log('user online: ' + users.length);
+    io.sockets.emit('user login', 
+      {'username': msg.username, 'data': 'user login'});//
+  });
+
+
+  //监听chat message事件
   socket.on('chat message', function(msg){
     console.log('message:' + msg);
     io.sockets.emit('chat message', msg);
   });
 
+  //监听断开连接事件，删除当前socket连接
   socket.on('disconnect', function(){
-    console.log('user disconnect');
+    for(var i=0; i<users.length; i++){
+      if(users[i].socket === socket){
+        console.log('user:' + users[i].name + ' disconnect');
+        io.sockets.emit('user disconnect', 
+          {username: users[i].name, data: 'user disconnect'});
+        users.splice(i, 1);
+      }
+    }
+    console.log('user online: ' + users.length);
   });
 
 
