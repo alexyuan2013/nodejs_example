20d6f5ba4e12c0af456e3017045a04cc293ee5ab
AlexYuan <alexyuan1988@gmail.com>
2016-10-14 16:25:18 +0800

    增加以room方式实现

--- socket.io/chat-example/index.js
+++ socket.io/chat-example/index.js
@@ -23,6 +23,11 @@ io.on('connection', function(socket){
       {'username': msg.username, 'data': 'user login'});//
   });
 
+  //监听加入room事件
+  socket.on('join', function(msg){
+    console.log('user:' + msg.username + ' join room');
+    socket.join(msg.username);
+  });
 
   //监听chat message事件
   socket.on('chat message', function(msg){
@@ -46,6 +51,17 @@ io.on('connection', function(socket){
 
 });
 
+
+//每隔五秒向每个room发送一个随机数（证明非广播）
+setInterval(function(){
+  //console.log("5s tick");
+  for(var i=0; i<users.length; i++){
+    var room = users[i].name;
+    var num = Math.floor(Math.random()*1000);
+    console.log(room + ' ' + num);
+    io.sockets.in(room).emit('random number', {username: 'Server', data: num});
+  }
+}, 60000);
 
 http.listen(3000, function(){
   console.log('listening on *: 3000');
