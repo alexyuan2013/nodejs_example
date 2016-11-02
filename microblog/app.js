3c007042d53ef8c30418bd652bc8d50851b5167e
AlexYuan <alexyuan1988@gmail.com>
2016-11-02 14:42:10 +0800

    add comment

--- microblog/app.js
+++ microblog/app.js
@@ -4,20 +4,20 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
+//routes是一个文件夹形式的本地模块，功能是为制定路径组织返回内容，相当于MVC架构中的控制器
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'views'));//静态文件支持
 app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
-app.use(bodyParser.json());
+app.use(bodyParser.json());//解析客户端的请求，通常是通过post发送的内容
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
