1bb09c36ec75309713d640d03035f3baab34a146
AlexYuan <alexyuan1988@gmail.com>
2016-12-01 14:53:44 +0800

    redis sorted set test

--- redis_test/index.js
+++ redis_test/index.js
@@ -10,18 +10,40 @@ var args = process.argv.slice(2);
 
 if(args == 'add'){
 	//≤‚ ‘sorted set add
-	redisClient.zadd('sorted_set', Date.now(), 'sorted set content1', redis.print);
-	redisClient.zadd('sorted_set', Date.now(), 'sorted set content2', redis.print);
-	redisClient.zadd('sorted_set', Date.now(), 'sorted set content3', redis.print);
-	redisClient.zadd('sorted_set', Date.now(), 'sorted set content4', redis.print);
-} else if(args == 'get'){
+	for(var i=0; i<100; i++){
+		redisClient.zadd('sorted_set', Date.now(), i + ' sorted set content', redis.print);
+	}
+} else if(args == 'getAll'){
 	//≤‚ ‘sorted set zrange
-	redisClient.zrange('sorted_set', 0, 10, 'withscores',function(err, replies){
+	redisClient.zrange('sorted_set', 0, -1, 'withscores', function(err, replies){
 		console.log(replies.length + ' replies');
 		replies.forEach(function(reply, i){
 			console.log(reply);
 			//console.log(i);
 		});
+	});
+} else if(args == 'get100'){
+	//≤‚ ‘sorted set zrange
+	redisClient.zrevrange('sorted_set', 0, 99, 'withscores', function(err, replies){
+		console.log(replies.length + ' replies');
+		replies.forEach(function(reply, i){
+			console.log(reply);
+			//console.log(i);
+		});
+	});
+} else if(args == 'getByScore'){
+	//≤‚ ‘sorted set zrange
+	redisClient.ZRANGEBYSCORE('sorted_set', 1480495510862, 1480495510862, 'withscores', function(err, replies){
+		console.log(replies.length + ' replies');
+		replies.forEach(function(reply, i){
+			console.log(reply);
+			//console.log(i);
+		});
+	});
+} else if(args == 'delByScore'){
+	//≤‚ ‘sorted set zrange
+	redisClient.ZREM('sorted_set', '1 sorted set content', function(err, reply){
+		console.log(reply + ' replies');
 	});
 } else {
 	console.log('Œﬁ–ßµƒ√¸¡Ó');
