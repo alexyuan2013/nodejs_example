538bd764d4cd7235743258946eb439d591988b1e
AlexYuan <alexyuan1988@gmail.com>
2016-12-06 10:12:55 +0800

    使用sorted set存储，有序消息列表

---
+++ socket.io/chat-example/index2_2.js
@@ -0,0 +1,171 @@
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var message = require('./lib/message2_2.js')();
+var bodyParser = require('body-parser');
+var constants = require('./lib/constants.js');
+
+//配置路由
+var router = express.Router();
+
+//配置访问静态文件
+app.use(express.static('public'));
+//配置解析json
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html')
+});
+
+io.on('connection', function(socket){
+  
+  //console.log('a user connected');
+
+  //监听登录事件，将username与websocket连接加入到列表中
+  socket.on('login', function(msg){
+    console.log('user: ' + msg.userID + ' login');
+    message.onlineUsers[msg.userID] = socket;
+    console.log('user online: ' + Object.keys(message.onlineUsers).length);
+    io.sockets.emit('userLogin', {'userID': msg.userID, 'data': 'user login'});//
+    //向用户发送未发消息队列
+    //message.sendMessagesWhenUserLogin(msg.userID);
+    socket.emit('loginSucceed',{});
+  });
+
+  //当客户端准备好后，开始发送消息
+  socket.on('beginSession', function(msg){
+    //向用户发送未发消息队列
+    message.sendMessagesWhenUserLogin(msg.userID);
+  });
+
+  //监听chat message事件，测试用
+  socket.on('chat message', function(msg){
+    console.log('message:' + msg);
+    var users = msg.data.split(',');
+    var content = 'random number: ' + Math.floor(Math.random()*1000);
+    console.log('users: ' + users);
+    message.sendMessageToUsers(users, content);
+  });
+
+  //监听断开连接事件，删除当前socket连接
+  socket.on('disconnect', function(){
+    for(var s in message.onlineUsers){
+      if(message.onlineUsers[s] === socket){
+        console.log('user:' + s + ' disconnect');
+        io.sockets.emit('user disconnect', {userID: s, data: 'user disconnect'});
+        delete message.onlineUsers[s];
+      }
+    }
+    console.log('user online: ' + Object.keys(message.onlineUsers).length);
+  });
+  
+  //监听回执receipt事件
+  socket.on('receipt', function(msg){
+    //收到回执，更新sendingMessages队列等
+    message.receiptMessage(msg.userID, msg.msgID);
+  });
+
+
+});
+
+
+//定时向每个连接发送一个随机数（证明非广播）
+setInterval(function(){
+  //console.log("5s tick");
+  for(var s in message.onlineUsers){
+    var num = Math.floor(Math.random()*1000);
+    message.onlineUsers[s].emit('random number', {userID: 'Server', data: num});
+  }
+}, 60000);
+//定时向每个连接发送一个随机数（广播）
+// setInterval(function(){
+//   //console.log("5s tick");
+//   io.sockets.emit('ALL', {'data': 'test all'});
+// }, 6000);
+
+/**
+ * REST接口，返回当前在线用户的id
+ */
+router.get('/onlineusers', function(req, res){
+  res.json(message.getOnlineUsersID());
+});
+
+/**
+ * REST接口，返回未发送消息列表
+ */
+router.get('/sendingMessages', function(req, res){
+  //定义callback函数，实际在getSendingMessages()中调用
+  var msgCallback = function(data){
+    res.json(data);
+  };
+  message.getSendingMessages(msgCallback);
+});
+
+/**
+ * REST接口，返回已发送消息列表
+ * 默认最多返回1000条数据
+ * redis的hash中数据是按写入的顺序不定
+ */
+router.get('/sentMessages', function(req, res){
+  //定义callback函数，实际在getSentMessages()中调用
+  var msgCallback = function(data){
+    res.json(data);
+  };
+  message.getSentMessages(0, constants.MAX_MSG_NUM-1, msgCallback);
+});
+
+/**
+ * 分页返回sentMessages
+ */
+router.get('/sentMessages/pages/:id', function(req, res){
+  try {
+    var page = parseInt(req.params.id) - 1;
+    var start = page*constants.PAGE_MSG_NUM;
+    var stop = (page+1)*constants.PAGE_MSG_NUM-1;
+    var msgCallback = function(data){
+      res.json(data);
+    };
+    message.getSentMessages(start, stop, msgCallback);
+  } catch(err){
+    console.log(err);
+    res.sendStatus(404);
+  }
+});
+
+/**
+ * REST接口，接收要发送的消息
+ */
+router.post('/message', function(req, res){
+  var users = req.body.users;
+  var content = req.body.content;
+  if(users == undefined || content == undefined){
+    res.send({"error":"消息格式错误"});
+    return;
+  }
+  if(message.sendMessageToUsers){
+    message.sendMessageToUsers(users, content);
+    res.sendStatus(200);
+  } else {
+    res.send({"error": "error info"});
+  }
+});
+
+/**
+ * REST接口，向在线用户广播数据
+ */
+router.post('/broadcast', function(req, res){
+  //var d = req.body.data;
+  io.sockets.emit('ALL', req.body);
+  res.sendStatus(200);
+});
+
+
+
+//所有接口使用api作为url的前缀
+app.use('/api', router);
+
+http.listen(3000, function(){
+  console.log('listening on *: 3000');
+});
---
+++ socket.io/chat-example/lib/message2_2.js
@@ -0,0 +1,191 @@
+/**
+ * message版本2-2——使用redis存储(sorted_set)
+ */
+function message(){
+  //获取redis模块
+  var redis = require('redis');
+  var constants = require('./constants.js');
+  //连接本地redis
+  var redisClient = redis.createClient();
+
+  //监听redis的error事件
+  redisClient.on("error", function (err) {
+      console.log("Error " + err);
+  });
+
+  /**
+   * 在线用户列表
+   * 示例：
+   * {"user_id1": socket,
+   *  "user_id2": socket
+   * }
+   */
+  this.onlineUsers = {};
+
+  /**
+   * 发送消息到用户列表
+   * users: [user_id1, user_id2]
+   * content: '消息内容' 
+   * */
+  this.sendMessageToUsers = function(users, content){
+    //add message to sendingMessages
+    var messageID = Date.now();
+    var usersState = {};
+    for(var u in users){
+      usersState[users[u]] = 0;
+    }
+    //redis消息内容，需要转化为string
+    var redisObj = {
+      'content': content,
+      'users':usersState,
+      'sentNum': 0
+    };
+    //写入到redis中，v0.0.2版中使用sorted set存储，有序
+    redisClient.zadd('sending_messages2', messageID, JSON.stringify(redisObj), redis.print);
+    //向在线用户发送
+    for(var u in users){
+      //用户在线
+      if(onlineUsers[users[u]] != undefined){
+        onlineUsers[users[u]].emit('newMessage', {msgID: messageID, data: content});
+        onlineUsers[users[u]].emit('EOM', {});
+      }
+    }
+  };
+
+  /**
+   * 发送消息列表到用户
+   * user: user_id1
+   */
+  this.sendMessagesWhenUserLogin = function(user){
+    //用户统计发送的条数，当数量达到100条时，发送EOM事件
+    var count=0;
+    //v0.0.2读取全部数据
+    redisClient.zrange('sending_messages2', 0, -1, 'withscores', function(err, replies){
+      if(err){
+        console.log(err);
+        return;
+      }
+      console.log(replies.length + ' replies');
+      var id = "",
+          content = {};
+      replies.forEach(function(reply, i){
+        //console.log(reply);
+        try {
+          if(i%2 == 0){
+            content = JSON.parse(reply);
+          } else {
+            id = reply;
+            if(onlineUsers[user] != undefined && content.users[user] == 0){
+              onlineUsers[user].emit('newMessage', {msgID: parseInt(id), data: content.content});
+              count=count+1;
+            }
+          }
+          if(count>0 && (i==replies.length-1 || count%constants.EOM_NUM == 0)) {
+            onlineUsers[user].emit('EOM', {});
+          }
+          //console.log(i);
+        } catch (err){
+          console.log(err);
+        }
+      });
+    });
+  };
+
+  /**
+   * 收到消息回执后的处理
+   * userID: user_id1
+   * msgID: message_id1
+   */
+  this.receiptMessage = function(userID, msgID){
+    redisClient.ZRANGEBYSCORE('sending_messages2', msgID, msgID, function(err, replies){
+      if(err){
+        console.log(err);
+        return;
+      }
+      console.log(replies.length + ' replies');
+      replies.forEach(function(reply, i){
+        try{
+          //console.log(reply);
+          var jsonValue = JSON.parse(reply);
+          if(jsonValue.users[userID]==0){//避免同一消息多次回执导致的计数错误
+            jsonValue.sentNum = jsonValue.sentNum + 1;
+          }
+          jsonValue.users[userID] = 1;
+          //jsonValue.sentNum = jsonValue.sentNum + 1;
+          //删除原来的一条
+          redisClient.ZREM('sending_messages2', reply, function(err, reply){
+            console.log(reply + ' removed');
+          });
+          //所有的人已发送，则写入sent_messages，并删除sending_messages的记录        
+          if(jsonValue.sentNum == Object.keys(jsonValue.users).length){
+            jsonValue.sentTime = Date.now();
+            redisClient.zadd('sent_messages2', msgID, JSON.stringify(jsonValue), redis.print);
+          } else {
+            //更新sendingMessages队列状态
+            redisClient.zadd('sending_messages2', msgID, JSON.stringify(jsonValue), redis.print);
+          }
+          //console.log(i);
+        } catch (err){
+          console.log(err);
+        }
+      });
+    });
+  };
+
+  //获取在线用户的id
+  this.getOnlineUsersID = function(){
+    return Object.keys(onlineUsers);
+  };
+
+  //获取未发消息列表
+  this.getSendingMessages = function(callback){
+    var messages = {};
+    //v0.0.2读取全部数据
+    redisClient.zrange('sending_messages2', 0, -1, 'withscores', function(err, replies){
+      if(err){
+        console.log(err);
+        return;
+      }
+      //console.log(replies.length + ' replies');
+      var id = "",
+          content = {};
+      replies.forEach(function(reply, i){
+        //onsole.log(reply);
+        if(i%2 == 0){
+          content = JSON.parse(reply);
+        } else {
+          id = reply;
+          messages[id] = content;
+        }
+        //console.log(i);
+      });
+      callback(messages);
+    });
+  };
+
+  //获取已发消息列表
+  this.getSentMessages = function(start, stop, callback){
+    var messages = {};
+    redisClient.zrevrange('sent_messages2', start, stop, 'withscores', function(err, replies){
+      console.log(replies.length + ' replies');
+      var id = "",
+          content = {};
+      replies.forEach(function(reply, i){
+        //console.log(reply);
+        if(i%2 == 0){
+          content = JSON.parse(reply);
+        } else {
+          id = reply;
+          messages[id] = content;
+        }
+        //console.log(i);
+      });
+      callback(messages);
+    });
+  };
+
+  return this;
+}
+
+//导出message模块
+module.exports = message;
