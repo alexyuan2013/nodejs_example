60c5336d2e7096674d6c7c85a7bb1a9449f9eef1
alexyuan <alexyuan1988@gmail.com>
2017-02-23 15:11:09 +0800

    分页显示，分批加载

--- socket.io/chat-example/public/js/push-ui/npush.js
+++ socket.io/chat-example/public/js/push-ui/npush.js
@@ -2,38 +2,81 @@ angular.module('npush', ['ui.bootstrap']);
 angular.module('npush', ['ui.bootstrap'])
 .controller('NPushController', function($scope, $http){
   $scope.users=[];
-  $http.get('http://localhost:3000/api/onlineusers/ipark').then(
-    function(result){
-      $scope.users = result.data || [];
-      //$scope.users = ['00061543', '00064623'];
-      //console.log(result);
-    },
-    function(error){
-      $scope.users = [];
+  $scope.page1 = {};
+  $scope.page1.start = 0;
+  $scope.page1.size = 10;
+  $scope.selectPage1 = function(){
+    $http.get('/api/onlineusers/ipark').then(
+      function(result){
+        $scope.users = result.data || [];
+        $scope.pageUsers = $scope.users.slice($scope.page1.start, $scope.page1.start + $scope.page1.size);
+        //$scope.users = ['00061543', '00064623'];
+        //console.log(result);
+      },
+      function(error){
+        $scope.users = [];
+      }
+    );
+  };
+  
+  $scope.$watch('page1.index', function(newValue, oldValue){
+    console.log(newValue);
+    if(newValue){
+      $scope.page1.start = $scope.page1.size * (newValue - 1);
+      $scope.pageUsers = $scope.users.slice($scope.page1.start, $scope.page1.start + $scope.page1.size);
     }
-  );
+  });
 
   $scope.sendingMessages = [];
-  $http.get('http://localhost:3000/api/sendingmessages/ipark').then(
-    function(result){
-      $scope.sendingMessages = result.data || [];
-      //console.log(result);
-    },
-    function(error){
-      $scope.sendingMessages = [];
+  $scope.page2 = {};
+  $scope.page2.start = 0;
+  $scope.page2.size = 10;
+  $scope.selectPage2 = function(){
+    $http.get('/api/sendingmessages/ipark').then(
+      function(result){
+        $scope.sendingMessages = result.data || [];
+        $scope.sendingMsgs = $scope.sendingMessages.slice($scope.page2.start, $scope.page2.start + $scope.page2.size);
+        //console.log(result);
+      },
+      function(error){
+        $scope.sendingMessages = [];
+      }
+    );
+  };
+
+  $scope.$watch('page2.index', function(newValue, oldValue){
+    console.log(newValue);
+    if(newValue){
+      $scope.page2.start = $scope.page2.size * (newValue - 1);
+      $scope.sendingmsgs = $scope.sendingMessages.slice($scope.page2.start, $scope.page2.start + $scope.page2.size);
     }
-  );
+  });
 
   $scope.sentMessages = [];
-  $http.get('http://localhost:3000/api/sentmessages/ipark').then(
-    function(result){
-      $scope.sentMessages = result.data || [];
-      //console.log(result);
-    },
-    function(error){
-      $scope.sentMessages = [];
-    }
-  );
+  $scope.page3 = {};
+  $scope.page3.start = 0;
+  $scope.page3.size = 10;
+  $scope.selectPage3 = function(){
+    $http.get('/api/sentmessages/ipark').then(
+      function(result){
+        $scope.sentMessages = result.data || [];
+        $scope.sentMsgs = $scope.sentMessages.slice($scope.page3.start, $scope.page3.start + $scope.page3.size);
+        //console.log(result);
+      },
+      function(error){
+        $scope.sentMessages = [];
+      }
+    );
+  };
+  
+  $scope.$watch('page3.index', function(newValue, oldValue){
+    console.log(newValue);
+    if(newValue){
+      $scope.page3.start = $scope.page3.size * (newValue-1);
+      $scope.sentMsgs = $scope.sentMessages.slice($scope.page3.start, $scope.page3.start + $scope.page3.size);
+    } 
+    
+  });
 
   $scope.submit = function() {
     var msg = {};
@@ -41,7 +84,7 @@ angular.module('npush', ['ui.bootstrap'])
     msg.users = [];
     msg.users = $scope.msgUsers.split(';');
     console.log(msg);
-    $http.post('http://localhost:3000/api/message/ipark', msg).then(
+    $http.post('/api/message/ipark', msg).then(
       function(result){
         console.log(result);
       },
